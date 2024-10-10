f567a7dbf7b53ae051ccc13d01ff202706f6ea5d
Dobby <119469809+Hexuan085@users.noreply.github.com>
2024-10-10 22:10:59 +0800

    Update readme.py

--- Registeration/readme.py
+++ Registeration/readme.py
@@ -1,19 +1,19 @@
 '''
-certification.py: 计算or协议
+certification.py: Calculation for OR protocol
 
-shishi.py: 画签名生成的时间
+shishi.py: Draw the time of signature generation
 
-sign_time.py: 计算签名生成的时间
+sign_time.py: Calculate the time of signature generation
 
-wmk_test.py: 生成membership
-A_post_add：acc的值
-S[x]:余数，用来生成和验证wit
+wmk_test.py: Generate the membership
 
-q_cycle.py: 生成群
+A_post_add: Calculate the value of acc
 
-acc_time.py: 画热力图
+q_cycle.py: Generate the group
 
-n: p*q 两个大素数相乘
+acc_time.py: Draw the acc generation time
+
+n: p*q
 
 
 '''
